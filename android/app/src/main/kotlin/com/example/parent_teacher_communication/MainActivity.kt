6e8f6d4aa70d77ce706750bc94e4e302ce67bce4
Banire Olaitan <olaitangoke@gmail.com>
2024-12-05 15:16:02 +0100

    Updated Code

--- android/app/src/main/kotlin/com/example/parent_teacher_communication/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.parent_teacher_communication
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- backend/config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-DATABASE_CONFIG = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': 'JasonZoe@1985',
-    'database': 'parent_teacher_db'
-}
